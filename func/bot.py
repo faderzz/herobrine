1290ef11b41fdb64704eefb886a709b7fe6a433f
faderz <58748338+faderzz@users.noreply.github.com>
2023-05-13 12:20:43 +0100

    Switch to prefix commands

--- func/bot.py
+++ func/bot.py
@@ -9,19 +9,19 @@ dotenv.load_dotenv()
 webhook_url = os.getenv('WEBHOOK_URL')
 token = os.getenv('DISCORD_TOKEN')
 
-### Bot
-client = commands.Bot(command_prefix='!',intents=discord.Intents.all())
+# Bot
+client = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 # Bot events
 @client.event
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
 
-@client.hybrid_command(name='hello', description='Say hello to the bot')
+@client.command(name='hello', description='Say hello to the bot')
 async def hello(ctx: commands.Context):
     await ctx.send('Hello!')
 
-@client.hybrid_command(name='add', description='Add two numbers')
+@client.command(name='add', description='Add two numbers')
 async def add(ctx: commands.Context, num1: int, num2: int):
     result = num1 + num2
     await ctx.send(f'The sum of {num1} and {num2} is {result}')
