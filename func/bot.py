23873bd9d4ba8c17f7f9343fa05119a31aa9f1af
faderz <58748338+faderzz@users.noreply.github.com>
2023-05-13 12:17:56 +0100

    Update bot.py

--- func/bot.py
+++ func/bot.py
@@ -1,5 +1,5 @@
-# Modules
 import discord
+from discord.ext import commands
 import sqlite3
 import os
 import dotenv
@@ -9,16 +9,22 @@ dotenv.load_dotenv()
 webhook_url = os.getenv('WEBHOOK_URL')
 token = os.getenv('DISCORD_TOKEN')
 
-# Bot functions
-intents = discord.Intents.default()
-intents.members = True
-intents.message_content = True
-client = discord.Client(intents=intents)
+### Bot
+client = commands.Bot(command_prefix='!',intents=discord.Intents.all())
 
-# On ready
+# Bot events
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{client.user.name} has connected to Discord!')
+
+@client.hybrid_command(name='hello', description='Say hello to the bot')
+async def hello(ctx: commands.Context):
+    await ctx.send('Hello!')
+
+@client.hybrid_command(name='add', description='Add two numbers')
+async def add(ctx: commands.Context, num1: int, num2: int):
+    result = num1 + num2
+    await ctx.send(f'The sum of {num1} and {num2} is {result}')
 
 # Run the client
 client.run(token)
